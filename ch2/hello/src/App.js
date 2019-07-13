9dc4d0e7d7b0ab7df5d1c59e9dfa845d9db76c13
kaepa3 <kaepa3@gmail.com>
2019-07-14 07:50:27 +0900

    end c2

--- ch2/hello/src/App.js
+++ ch2/hello/src/App.js
@@ -1,12 +1,11 @@
-import React, { Component } from 'react'
-import './App.css'
+import React, {Component} from 'react';
+import './App.css';
 
 class App extends Component {
   render () {
     return <div className='App'>
-      <h2>塔を建てる時は、まず座って費用を計算しよう</h2>
-    </div>
+        <h2>等を建てる時は、まず金</h2>
+      </div>
   }
 }
-
-export default App
+export default App;
